d4df39b32736907018a9a22ef5e49c6ad9b2efc0
Ashley Smith <ash.mash.134@gmail.com>
2020-11-26 22:26:31 +1100

    Moved AboutMe sections into their own folder to keep things organised

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import MyStory from './MyStory'
-import Skills from './Skills'
-import Interests from './Interests'
+import MyStory from './aboutMeSections/MyStory'
+import Skills from './aboutMeSections/Skills'
+import Interests from './aboutMeSections/Interests'
 
 const AboutMe = () => {
   return (
